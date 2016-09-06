3b0f99c0e5dc8529d865a88327397126ac7a5e35
Andy Gregorowicz <andy@mitre.org>
2016-09-06 11:50:23 -0400

    Added the first real test

---
+++ test/components/measure_category_test.js
@@ -0,0 +1,22 @@
+import { renderComponent, expect } from '../test_helper';
+import MeasureCategory from '../../src/components/MeasureCategory';
+
+describe('MeasureCategory', () => {
+  let component;
+
+  beforeEach(() => {
+    const props = {
+      category: "Core",
+      measures: [{cmsId: "123v1", name: "Controlling BP", description: "", hqmfId: "1234", category: "Core"},
+                 {cmsId: "124v1", name: "Controlling Weight", description: "", hqmfId: "5678", category: "Core"}],
+      onAddMeasure: (measure) => {
+        expect(measure.hqmfId).to.equal("1234");
+      }
+    };
+    component = renderComponent(MeasureCategory, props);
+  });
+
+  it('will display both measures', () => {
+    expect(component.find("button").length).to.equal(3); // two measures + select all
+  });
+});
--- test/initTest.js
+++
@@ -1,7 +0,0 @@
-var assert = require('assert');
-
-describe('root', function() {
-  it('2+2 should equal 4', function() {
-    assert.equal(4, 2+2);
-  });
-});
