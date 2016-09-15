6b3e0fed4f1045cbc37fb580469dba65d2b25641
Andy Gregorowicz <andy@mitre.org>
2016-09-15 13:36:40 -0400

    Removing references to popHealth

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
         <div className="container">
           <div className="navbar-header col-lg-4">
             <Link className="logo" to="/">
-              <img alt="Pophealth logo small trans" src="/assets/images/popHealth-logo-3x.png" width="100" />
+              <img alt="logo small trans" src="/assets/images/logo-3x.png" width="100" />
             </Link>
           </div>
           <div className="collapse navbar-collapse in">
